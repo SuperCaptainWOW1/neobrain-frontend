744dc6c2764232c8149f7c6450c5e49a8073185a
Vladimir Makhortov <makhortov.dev@gmail.com>
2020-04-03 18:51:13 +0300

    Project init

---
+++ src/views/Chat.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="chat">
+    <h1>Welcome to the chat, buddy</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  components: {
+  },
+};
+</script>
